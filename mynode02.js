4441c9036eafd41133d67542583e296ccf36c9a9
Yves Hoebeke <yves.hoebeke@gmail.com>
2015-03-21 11:06:50 -0400

    Update mynode02.js
    todo: check why extending native String object is not doing its job...

--- mynode02.js
+++ mynode02.js
@@ -1,6 +1,19 @@
 var mynode02Something = "express train to Timboktu";
 
+/*
+String.prototype.capitalizeFirstLetter = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+}
+*/
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 exports.sayHello = function(name){
-	var mystring = "Hello " + name + ", are you taking the " + mynode02Something + "?";
+	if (typeof name === 'undefined') { name = "bob" }
+
+	var mystring = "Hello " + capitalizeFirstLetter(name) + ", are you taking the " + mynode02Something + "?";
 	console.log(mystring);
 }
+
