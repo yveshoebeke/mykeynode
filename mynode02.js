a76aea7ca5cc1a982b289c2c1731acacaa9b5226
Yves Hoebeke <yves.hoebeke@gmail.com>
2015-03-21 10:47:05 -0400

    Create mynode02.js

---
+++ mynode02.js
@@ -0,0 +1,6 @@
+var mynode02Something = "express train to Timboktu";
+
+exports.sayHello = function(name){
+	var mystring = "Hello " + name + ", are you taking the " + mynode02Something + "?";
+	console.log(mystring);
+}
